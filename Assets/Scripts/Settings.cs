66f56c47d53998a7b206254af2cad1b2196b3c56
ilya034 <i5304093@gmail.com>
2025-05-12 16:58:15 +0500

    Add settings script

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Back()
+    {
+        CustomSceneManager.LoadPreviousScene();
+    }
+}
