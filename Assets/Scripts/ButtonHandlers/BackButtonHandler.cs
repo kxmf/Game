41436b008c25cffa9cf86fc27ffc1407f98dd45a
ilya034 <i5304093@gmail.com>
2025-06-10 17:08:55 +0500

    delete all

--- Assets/Scripts/ButtonHandlers/BackButtonHandler.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class BackButtonHandler : MonoBehaviour
-{
-    public void LoadPreviousScene() => CustomSceneManager.LoadPreviousScene();
-}
--- Assets/Scripts/ButtonHandlers/SettingsButtonHandler.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class SettingsButtonHandler : MonoBehaviour
-{
-    public void LoadSettingsScene() => CustomSceneManager.LoadScene("Settings");
-}
